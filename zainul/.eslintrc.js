8e9b701a9b5697bbc844998df96c0a707e32b98e
KRTirtho <krtirtho@gmail.com>
2021-04-10 19:09:59 +0600

    [titumir][zainul]⚛️👕add/modify both project's prettier & eslint. Check Grommet in App.ts

---
+++ zainul/.eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  extends: [
+    "react-app",
+    "react-app/jest",
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+    'plugin:prettier/recommended'
+  ],
+  plugins: ['react', '@typescript-eslint', 'jest'],
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  ignorePatterns: ['.eslintrc.js', "./src/reportWebVitals.ts"],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+  },
+  rules: {
+    'linebreak-style': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    "react/react-in-jsx-scope": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off"
+  },
+};
--- zainul/src/App.tsx
+++ zainul/src/App.tsx
@@ -1,25 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Grommet, Box, Paragraph } from 'grommet';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+    <Grommet full>
+      <Box>
+        <Paragraph>
           Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        </Paragraph>
+        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
-      </header>
-    </div>
+      </Box>
+    </Grommet>
   );
 }
 

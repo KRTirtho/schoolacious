e35026c5a94285504d0e36303e5381a9e3a65853
KRTirtho <krtirtho@gmail.com>
2021-06-19 09:27:14 +0600

    [titumir]✅ Todo Tests  for grades & sections

--- titumir/test/functional/grade.e2e-spec.ts
+++ titumir/test/functional/grade.e2e-spec.ts
@@ -5,7 +5,7 @@ import request from "supertest";
 import { AppModule } from "../../src/app.module";
 import { bootstrapApp } from "../e2e-test.util";
 
-describe("(e2e) PATH: ", () => {
+describe("(e2e) PATH: /school/:school/grade", () => {
     let app: INestApplication;
     let server: Server;
     let client: request.SuperTest<request.Test>;
@@ -21,6 +21,39 @@ describe("(e2e) PATH: ", () => {
         server = app.getHttpServer();
         client = request(server);
     });
+
+    test.todo("/ (POST) perfectly create grades");
+    test.todo("/ (POST) with non-applicable user");
+    test.todo("/ (POST) empty array");
+    test.todo("/ (POST) wrong array elements");
+    test.todo("/ (POST) co-admin auth");
+    test.todo("/ (POST) duplicate");
+
+    test.todo("/ (GET) perfectly get all grades");
+    test.todo("/ (GET) with non-applicable user");
+
+    test.todo("/:grade (GET) perfectly get a specific grade");
+    test.todo("/:grade (GET) wrong grade");
+
+    test.todo("/:grade/subject (POST) perfectly assign subjects");
+    test.todo("/:grade/subject (POST) with non-applicable user");
+    test.todo("/:grade/subject (POST) empty array");
+    test.todo("/:grade/subject (POST) wrong array elements");
+    test.todo("/:grade/subject (POST) co-admin auth");
+    test.todo("/:grade/subject (POST) grade-moderator auth");
+    test.todo("/:grade/subject (POST) put");
+
+    test.todo("/:grade/assign-moderator (PUT) perfectly");
+    test.todo("/:grade/assign-moderator (PUT) with non-applicable user");
+    test.todo("/:grade/assign-moderator (PUT) no required fields");
+    test.todo("/:grade/assign-moderator (PUT) wrong body");
+    test.todo("/:grade/assign-moderator (PUT) already grade-moderator");
+
+    test.todo("/:grade/assign-examiner (PUT) perfectly");
+    test.todo("/:grade/assign-examiner (PUT) with non-applicable user");
+    test.todo("/:grade/assign-examiner (PUT) no required fields");
+    test.todo("/:grade/assign-examiner (PUT) wrong body");
+    test.todo("/:grade/assign-examiner (PUT) already grade-examiner");
 
     afterAll(async () => {
         await app.close();
